55368146fddde904ada132d8d77d9470e04c9399
JBuckley13 <72932674+JBuckley13@users.noreply.github.com>
2021-10-21 15:24:55 +0100

    SavingsAccount.java for Lab 3 Question 2

---
+++ jBuckley/src/ie/itcarlow/lab3/SavingsAccount.java
@@ -0,0 +1,64 @@
+package ie.itcarlow.lab3;
+
+//Student Name 		: 	Julie Buckley
+//Student Id Number	: 	C00200976
+//Date 				: 	21/10/2021
+//Purpose 			: 	Lab 3 Question 2
+
+public class SavingsAccount {
+	
+	private static int numberOfAccounts = 0; //
+	public static double annualInterestRate;
+	private double savingsBalance;
+	private int accountNumber;
+
+	public SavingsAccount() // Constructor
+	{
+		numberOfAccounts++;
+		accountNumber = numberOfAccounts;
+		annualInterestRate = 0.0;
+		savingsBalance = 0.0;
+	}
+	
+	public SavingsAccount(double interestR, double savings)
+	{
+		numberOfAccounts++;
+		accountNumber = numberOfAccounts;
+		annualInterestRate = interestR;
+		savingsBalance = savings;
+	}
+	
+	public static void modifyInterestRate(double interestR)
+	{
+		annualInterestRate = interestR;
+	}
+	
+	public void setSavingsBalance(double savings)
+	{
+		savingsBalance = savings;
+	}
+	
+	public double calculateMonthlyInterest()
+	{
+		double monthlyInterest;
+		
+		monthlyInterest = (savingsBalance * (annualInterestRate/12));
+		savingsBalance = savingsBalance + monthlyInterest;
+		return monthlyInterest;
+	}
+	
+	public int getAccountNumber()
+	{
+		return accountNumber;
+	}
+	
+	public double getSavingsBalance()
+	{
+		return savingsBalance;
+	}
+	
+	public double getAnnualInterestRate()
+	{
+		return annualInterestRate;
+	}
+}
